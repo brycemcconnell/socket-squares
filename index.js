329bdc71a0efa91f86a75b436ff02d185749a0ab
brycemcconnell <brycemcconnell@gmail.com>
2018-03-22 00:06:23 +1100

    Moved index.html into public

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const random = (max, min) => {
 };
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 app.use(express.static('public'));
 
